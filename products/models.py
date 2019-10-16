6815e6e7d3f32194d6ae7204b6a10e9fa1a17b2a
dano5342 <ellis.daniel0@gmail.com>
2019-10-16 13:03:12 +0200

    annoying-django imported - this functionality should help with autocreating the users name in product display/posts, category model added, this may require its own app. Migrations run, admin registered to view and see the product working with the category

--- products/models.py
+++ products/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from annoying.fields import AutoOneToOneField
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    category = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.category
 
 class Product(models.Model):
     title = models.CharField(max_length=40)
     summary = models.CharField(max_length=120)
     description = models.TextField(max_length=500, blank=False)
-    sold_by = models.OneToOneField(User, on_delete=models.CASCADE)
+    sold_by = AutoOneToOneField(User, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="")
 
     def __str__(self):
         return self.title
