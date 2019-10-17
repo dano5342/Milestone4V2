8019f5a1e6226f4054ca78ebbafce3f6ede0f82c
dano5342 <ellis.daniel0@gmail.com>
2019-10-17 10:38:08 +0200

    Removed old migrations, AutoOneToOneField was creating issues where only 1 item could be sold per user, this is fixed

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     title = models.CharField(max_length=40)
     summary = models.CharField(max_length=120)
     description = models.TextField(max_length=500, blank=False)
-    sold_by = AutoOneToOneField(User, on_delete=models.CASCADE)
+    sold_by = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default="")
 
