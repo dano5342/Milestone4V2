0618e11e5fe2c4ed0086b80039c72c03148ed1b2
dano5342 <ellis.daniel0@gmail.com>
2019-10-30 14:44:24 +0100

    expanded search functionality to include searching the summary, description and not just the title, potentially could do this with the assigned categories too

--- search/views.py
+++ search/views.py
@@ -3,6 +3,9 @@ from products.models import Product
 
 
 def search(request):
-    products = Product.objects.filter(title__icontains=request.GET['q'])
+    pof = Product.objects.filter
+    products = (pof(title__icontains=request.GET['q'])|
+                pof(summary__icontains=request.GET['q'])|
+                pof(description__icontains=request.GET['q']))
     context = {'products': products}
     return render(request, 'all_products.html', context)
