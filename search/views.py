a24f36e3459fd67313929de47f35ce58b05c516b
dano5342 <ellis.daniel0@gmail.com>
2019-10-30 15:13:09 +0100

    solved searching functionality removing the Categories from the navbar, this now needs removing from the profile and accounts area

--- search/views.py
+++ search/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 
 
 def search(request):
+    category = Category.objects.all()
     pof = Product.objects.filter
     products = (pof(title__icontains=request.GET['q'])|
                 pof(summary__icontains=request.GET['q'])|
                 pof(description__icontains=request.GET['q']))
-    context = {'products': products}
+    context = {'products': products, 'category': category}
     return render(request, 'all_products.html', context)
