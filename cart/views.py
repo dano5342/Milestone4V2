e6b276190b2ed0e214f4d7d66ad540104fc50b32
dano5342 <ellis.daniel0@gmail.com>
2019-10-31 11:18:58 +0100

    basic cart html page loaded in, this will be changed to suit my needs and design spec. Found a bug where the cart items weren't loading in, this was due to a duplicate action within the form code, changed index to home as named appropriately

--- cart/views.py
+++ cart/views.py
@@ -23,7 +23,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('home'))
 
 
 def remove_product(request, id):
