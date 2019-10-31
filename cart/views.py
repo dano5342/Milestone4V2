f1a6cdad2a3f4c632de94d5f88c1929e103ac96e
dano5342 <ellis.daniel0@gmail.com>
2019-10-31 10:20:32 +0100

    Added functionality for cart app, written the views and the context for the cart. HTML to be written after view testing and URLS have been done

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def view_cart(request):
+    """
+    A view that renders the Users cart items
+    Storing them for payment at checkout later
+    """
+    return render(request, 'cart.html')
+
+
+def add_to_cart(request, id):
+    """
+    Add a quantity of the product to the users cart
+    """
+    
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def remove_product(request, id):
+    """
+    View for button removing items from cart from the users stored items
+    """
+    cart = request.session.get('cart', {})
+    del cart[id]
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+
+def adjust_products(request, id):
+    """
+    Adjusts the users amount stored in cart of a specified
+    product within that cart session
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart  = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else: 
+        cart.pop(id)
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
