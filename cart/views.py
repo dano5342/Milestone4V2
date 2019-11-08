3ab8184df970af599b4734a4e0d85fcb14cde565
dano5342 <ellis.daniel0@gmail.com>
2019-11-08 13:47:41 +0100

    added the URL for add to cart ,apparently had for got to do this before. routed the add to cart redirect to all products, will add a message in next

--- cart/views.py
+++ cart/views.py
@@ -23,7 +23,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('home'))
+    return redirect(reverse('products'))
 
 
 def remove_product(request, id):
