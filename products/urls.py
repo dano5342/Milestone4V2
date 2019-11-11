00097aa758cb04a417cd7344d5ffa4580cd26e49
dano5342 <ellis.daniel0@gmail.com>
2019-11-11 12:10:40 +0100

    Added a detailed view for more info, this isnt working as allproducts is returning a category in the view. Will look into this on the other pc, stashing for now. take a look at the view route?

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import more_info, all_products
+from .views import  all_products, MoreInfo #more_info,
 
 
 urlpatterns = [
     path('', all_products, name="products"),
-    re_path(r'^(?P<product_id>\d+)/$', more_info, name="more info"),
-    re_path(r'^(?P<category>[-\w]+)/$', all_products, name="products")
+    #re_path(r'^(?P<product_id>\d+)/$', more_info, name="more info"),
+    re_path(r'^(?P<category>[-\w]+)/$', all_products, name="products"),
+    path('more_info/<int:pk>', MoreInfo.as_view(), name="more info")
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Product, Category
+from django.views.generic import DetailView
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
@@ -11,8 +12,6 @@ def all_products(request, category_name=None):
     products = Product.objects.all()
     category = Category.objects.all()
     selected = None
-
-
 
     if category_name:
         category = get_object_or_404(Category, category_name)
@@ -39,14 +38,42 @@ def all_products(request, category_name=None):
     return render(request, 'all_products.html', args)
 
 
-def more_info(request, product_id):
+class MoreInfo(DetailView):
+
+
+    model = Product
+    template_name = 'prod_detail.html'
+    context_object_name = 'product'
+    extra_context = {}
+
+
+    def get_object(self, queryset=Product):
+        _id = self.kwargs.get('pk')
+        instance = get_object_or_404(Product, id=_id)
+
+        self.extra_context['product'] = instance
+        self.extra_context['more_prods'] = Product.objects.all().filter(category=instance.category).exclude(id=_id).order_by('-pubd')[:6]
+
+        return instance
+
+
+
+    def post(self, request, *args, **kwargs):
+        _id = self.kwargs.get('pk')
+        instance = get_object_or_404(Product, id=_id)
+        product = self.get_object()
+        category = self.category
+        return redirect(reverse('prod_detail.html', kwargs={'pk': product.pk}))
+
+        
+#def more_info(request, product_id):
     """
     This view is for seeing more detail into the product
     The detailed description is removed from the homepage
     so it is added here.
     """
-    product = get_object_or_404(Product, pk=product_id)
-    category = Category.objects.all()
+    #product = get_object_or_404(Product, pk=product_id)
+    #category = Category.objects.all()
     #more_products = Product.objects.filter(category=category)[:6]
-    detailed_args = {'product': product, 'product_id': product_id} #'more_products': more_products}
-    return render(request, "prod_detail.html", detailed_args)
+    #detailed_args = {'product': product, 'product_id': product_id} #'more_products': more_products}
+    #return render(request, "prod_detail.html", detailed_args)
