8bc24fe3f3f958f12841dd5cf2846b30e2d77a39
dano5342 <ellis.daniel0@gmail.com>
2019-11-14 13:21:32 +0100

    amended the messages to include more information. Card styling now standardised and removed the add to product form from the all products page. Trying if statements for empty cart page

--- cart/views.py
+++ cart/views.py
@@ -22,7 +22,7 @@ def add_to_cart(request, id):
         cart[id] = int(cart[id]) + quantity
     else:
         cart[id] = cart.get(id, quantity)
-    messages.success(request, '%s was added to the cart' % (product.title))
+    messages.success(request, '%s x %s was added to the cart' % (quantity ,product.title))
     request.session['cart'] = cart
     return redirect(reverse('products'))
 
@@ -31,9 +31,11 @@ def remove_product(request, id):
     """
     View for button removing items from cart from the users stored items
     """
+    product = get_object_or_404(Product, pk=id)
     cart = request.session.get('cart', {})
     del cart[id]
     request.session['cart'] = cart
+    messages.success(request, '%s was removed from the cart' %(product.title))
     return redirect('view_cart')
 
 
@@ -42,6 +44,7 @@ def adjust_products(request, id):
     Adjusts the users amount stored in cart of a specified
     product within that cart session
     """
+    product = get_object_or_404(Product, pk=id)
     quantity = int(request.POST.get('quantity'))
     cart  = request.session.get('cart', {})
 
@@ -50,4 +53,5 @@ def adjust_products(request, id):
     else: 
         cart.pop(id)
     request.session['cart'] = cart
+    messages.success(request, '%s was amended to %s' %(product.title, quantity))
     return redirect(reverse('view_cart'))
