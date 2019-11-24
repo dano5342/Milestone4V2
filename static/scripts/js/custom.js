d3b3114fe307f1615fbf665dda10d5f6b3eaf295
dano5342 <ellis.daniel0@gmail.com>
2019-11-24 12:32:01 +0100

    Styled buttons to personal styles, removed defunct JS code from the navbar, sticking with a static navbar now. Added extra color mixins and trialling these on buttons for affect. Keywords added for Search Engine optimization.

--- static/scripts/js/custom.js
+++ static/scripts/js/custom.js
@@ -1,21 +1,8 @@
 
-/*
+
 $('document').ready(function(){
-  var stickyNavTop = $('#navbar').offset().top;
-  var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > stickyNavTop){
-      $('#navbar').addClass('sticky'); 
-    }else {
-      $('#navbar').removeClass('sticky');
-    }
-  };
-
-  //Call the sticky navbar function
-stickyNav();
-$(window).scroll(function(){
-  stickyNav();
+  setTimeout(function() {
+    $('#messageSection').fadeOut(5000);
 });
 
 });
-*/ 
