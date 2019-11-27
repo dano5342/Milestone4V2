247ef06f18a0d8d083e8635b5c9c6fcb5163fbf0
dano5342 <ellis.daniel0@gmail.com>
2019-11-27 13:35:57 +0100

    Finally sorted out the FA cart. This is now dynamically updated with the # of items in the cart. Displaying correctly and is checked on all screen sizes

--- ms4/settings.py
+++ ms4/settings.py
@@ -150,8 +150,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+#STATICFILES_LOCATION = 'static'
+#STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
