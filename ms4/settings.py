dfb1a86c768874fd793ba5afa8876d2c59d03522
dano5342 <ellis.daniel0@gmail.com>
2019-11-27 13:15:34 +0100

    added in reset password functionality to the application by following Corey Shafer on youtube, styled the same as the login/register pages etc for the reset forms, routed these into the app via the main urls and the tempalte are in the accounts app

--- ms4/settings.py
+++ ms4/settings.py
@@ -175,3 +175,10 @@ STRIPE_SECRET = os.environ.get('STRIPE_SECRET')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
