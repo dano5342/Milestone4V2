dd53a126a60406dcb3463c61faf260b688d62e4b
dano5342 <ellis.daniel0@gmail.com>
2019-11-28 15:58:18 +0100

    Removed CSRF token from the category filtering section. Product tests completed, had some issues creating the final test for the detailed view TDD dictated that I should write the smallest amount of code here to get it to test out properly, This will be copied into the unit testing write up of the readme

--- products/testing/test_views.py
+++ products/testing/test_views.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from products.models import Product, Category
 
-# Create your tests here.
+class TestProductPages(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+
+
+    def test_allproduct_view_without_filter(self):
+        page = self.client.get('/products/')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'all_products.html')
+
+
+    """
+    Check that pagination function works correctly with the same
+    template as the main allproduct view
+    """
+    def test_pagination_checker(self):
+        page = self.client.get('/products/?page=3')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'all_products.html')
+
+
+    def test_category_functionality(self):
+        page = self.client.get('/products/?category=Furniture')
+        self.assertEqual(page.status_code, 200)
+
+
+    def test_detail_prod_view(self):
+        """
+        Create a category, create a product with that category
+        try and access the page for this product.
+        """
+        cat = Category(category="testCat")
+        cat.save()
+        #testing this function started here, having to create a
+        #product and then making sure the NOT NULL fields were
+        #Filled and then the category object had its relational item
+        #in another area of the DB
+        product = Product(title = 'TestProd',
+                          price = 24,
+                          category = cat)
+        product.save()
+        id = product.id
+        page = self.client.get('/products/more_info/{0}'.format(id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'prod_detail.html')
