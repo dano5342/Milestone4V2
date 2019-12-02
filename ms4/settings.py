2c31381d9b0897ef29437693f907949795c574cc
dano5342 <ellis.daniel0@gmail.com>
2019-12-02 16:50:22 +0100

    Added titles to the product html pages, also found a bug when going through the register route, so have changed the DJANGO.settings folder back to its original removing the docstring, this cannot be parsed through on a multiline so it shall be PEP-8 Erroneous

--- ms4/settings.py
+++ ms4/settings.py
@@ -106,9 +106,7 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        """django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator""",
+        'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
     },
     {
         'NAME':
