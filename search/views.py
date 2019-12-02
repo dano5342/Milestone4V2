943946b95487a4a633bbe26ba3b1b2f14269bf40
dano5342 <ellis.daniel0@gmail.com>
2019-12-02 16:53:51 +0100

    Search app PEP8 compliant, the customstorages and the manage.py files checked as well. Validation 98% complete, need to double check Coverage and the Readme.md

--- search/views.py
+++ search/views.py
@@ -5,8 +5,8 @@ from products.models import Product, Category
 def search(request):
     category = Category.objects.all()
     pof = Product.objects.filter
-    products = (pof(title__icontains=request.GET['q'])|
-                pof(summary__icontains=request.GET['q'])|
+    products = (pof(title__icontains=request.GET['q']) |
+                pof(summary__icontains=request.GET['q']) |
                 pof(description__icontains=request.GET['q']))
     context = {'products': products, 'category': category}
     return render(request, 'all_products.html', context)
